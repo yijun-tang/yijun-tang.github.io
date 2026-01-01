9cc64aa3370466e4963da30d28d8e502e5f167ae
Yijun Tang <yijuntang94@gmail.com>
2026-01-01 14:54:39 +0800

    update project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,18 +3,30 @@ import SectionHeader from "./common/section-header";
 
 export default function Projects() {
   return (
-    <div
-      className="content-stretch flex flex-col gap-3 sm:gap-4 items-start relative w-full"
-    >
+    <div className="content-stretch flex flex-col gap-3 sm:gap-4 items-start relative w-full">
       <SectionHeader title="Projects" />
-      <div className="grid grid-cols-1 gap-4 sm:gap-5 w-full">
+      <div className="grid grid-cols-1 gap-1 w-full">
+        <a
+          className="block"
+          href="https://selectedai.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p
+            className={`${linkFont.className} text-[18px] leading-snug hover:underline underline-offset-2 decoration-1`}
+          >
+            Selected: AI Writing Assistant for Productivity
+          </p>
+        </a>
         <a
           className="block"
           href="https://github.com/yijun-tang/rudis"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <p className={`${linkFont.className} text-base sm:text-lg md:text-xl leading-snug hover:underline underline-offset-2 decoration-1`}>
+          <p
+            className={`${linkFont.className} text-[18px] leading-snug hover:underline underline-offset-2 decoration-1`}
+          >
             rudis: Redis Re-implemention in Rust for Learning Purpose
           </p>
         </a>
